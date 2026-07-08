d2d3955030549dc738231ceddd57375fe06e0a09
dhruvitrada <dhruvitrada18@gmail.com>
2026-07-08 22:43:11 +0530

    Update prac1E.cpp

---
+++ prac1E.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+    int m1,m2,m3,m4,m5,m6; 
+    cout << "Enter marks of six subjects :"<<endl; 
+    cin>>m1>>m2>>m3>>m4>>m5>>m6; 
+    cout<<"sum of marks :"<<m1+m2+m3+m4+m5+m6; 
+    
+    return 0; 
+
+} 
