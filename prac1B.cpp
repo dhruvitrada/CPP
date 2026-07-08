8a1cad36c942c6db22859dc1718405dc3f70eb5d
dhruvitrada <dhruvitrada18@gmail.com>
2026-07-08 22:41:35 +0530

    Update prac1B.cpp

--- prac1B.cpp
+++ prac1B.cpp
@@ -1,9 +1,43 @@
-// Online C++ compiler to run C++ program online
-#include <iostream>
-using namespace std;
-int main() {
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+
+    int num1,num2,op; 
+
+    cout << "Enter two numbers :"<<endl; 
+    cin>>num1>>num2; 
     
-    cout<<"Enter number of digits : ";
-    
-    return 0;
-}
+    cout<<"Enter the operation :\n 1.Addition \n 2. SUbtraction \n 3. Multiplication \n 4. Division\n"; 
+    cin>>op; 
+
+    if(op==1) 
+    { 
+        cout<<"Result is :"<<num1+num2; 
+    } 
+    else if(op==2) 
+    { 
+        cout<<"Result is :"<<num1-num2; 
+    } 
+    else if(op==3) 
+    { 
+        cout<<"Result is :"<<num1*num2; 
+    } 
+    else if(op==4) 
+    { 
+        if(num2!=0) 
+        { 
+        cout<<"Result is :"<<num1/num2; 
+        } 
+        else 
+        { 
+            cout<<"Not Defined!!"; 
+        } 
+    } 
+    else 
+    { 
+        cout<<"Operation Invalid!!"; 
+    } 
+
+    return 0; 
+} 
