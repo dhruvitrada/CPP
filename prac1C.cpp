6592894723c009a1ea72e0dc33f3687a33cc0cda
dhruvitrada <dhruvitrada18@gmail.com>
2026-07-08 22:42:08 +0530

    Update prac1C.cpp

---
+++ prac1C.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+    float c,f; 
+    cout << "Enter value in fahrenheit :"<<endl; 
+    cin>>f; 
+
+    c=(f-32)/1.8; 
+
+    cout << "Value of Celcius :"<<c; 
+
+    return 0; 
+} 
